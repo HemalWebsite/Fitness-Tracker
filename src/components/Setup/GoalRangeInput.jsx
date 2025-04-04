0d929f4ff927638e67f08bbb17ca28e1fde628ad
HemalWebsite <142029355+HemalWebsite@users.noreply.github.com>
2025-04-04 06:00:50 +0530

    Add files via upload

---
+++ src/components/Setup/GoalRangeInput.jsx
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react';
+import { Form, Row, Col } from 'react-bootstrap';
+
+const GoalRangeInput = ({ goalType, onSave }) => {
+  const [minValue, setMinValue] = useState('');
+  const [maxValue, setMaxValue] = useState('');
+
+
+  useEffect(() => {
+    if (minValue !== '' && maxValue !== '') {
+      onSave(goalType, minValue, maxValue);
+    }
+  }, [minValue, maxValue, goalType, onSave]);
+
+  const handleMinChange = (e) => {
+    setMinValue(e.target.value);
+  };
+
+  const handleMaxChange = (e) => {
+    setMaxValue(e.target.value);
+  };
+
+  return (
+    <Row>
+      <Col md={5}>
+        <Form.Control
+          type="number"
+          placeholder="Min"
+          value={minValue}
+          onChange={handleMinChange}
+        />
+      </Col>
+      <Col md={2} className="text-center">
+        <h2>x</h2>
+      </Col>
+      <Col md={5}>
+        <Form.Control
+          type="number"
+          placeholder="Max"
+          value={maxValue}
+          onChange={handleMaxChange}
+        />
+      </Col>
+    </Row>
+  );
+};
+
+export default GoalRangeInput;
